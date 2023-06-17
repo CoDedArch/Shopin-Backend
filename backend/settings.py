3ed33ea623463aabd1ccd6f41ef417726f5550df
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-06-17 22:44:24 +0000

    make config and add views to handle url

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -61,7 +60,7 @@ ROOT_URLCONF = os.environ.get('ROOT_URLCONF')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["%s/templates/"%BASE_DIR],
         # will be changing this env 
         'APP_DIRS': int(os.environ.get('APP_DIRS')),
         'OPTIONS': {
@@ -129,3 +128,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- shopin/views.py
+++ shopin/views.py
@@ -1,3 +1,18 @@
+"""
+    Module is tasked with handling the business
+    logic for all the views that come to this site
+"""
+from django.conf import settings
 from django.shortcuts import render
+import os
 
 # Create your views here.
+def home_page(request):
+    """
+        Returns the home page for Shopin
+        Since this i will be loading a template
+        i will not be using an HttpResponse 
+        but a render 
+    """
+    pass    
+
