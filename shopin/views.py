fd9a7e99187be5d237f5040c07b3767b1cf86c0f
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-06-24 10:11:04 +0000

    add a new function to handle business logic of new endpoint

--- shopin/views.py
+++ shopin/views.py
@@ -3,21 +3,21 @@
     logic for all the views that come to this site
     MY VIEW IS VERY NOISE WITH UNWANTED
 """
-from django.conf import settings
 from django.shortcuts import render
-import os
+from django.http import HttpResponse
+
 
 # Create your views here.
 def home_page(request):
     """
         Returns the home page for Shopin
         Since this i will be loading a template
-        i will not be using an HttpResponse 
-        but a render 
+        i will not be using an HttpResponse
+        but a render
     """
     SD_SHOP = 'A very simple shopin template for ' +\
-    'show casing a description to be in the name of laughter'
-    LD_SHOP = 'A very simple shopin template for ' 
+        'show casing a description to be in the name of laughter'
+    LD_SHOP = 'A very simple shopin template for '
     'show casing a long description of a shop description'
     list_of_shops = [{
         'Title': 'Shopin 1',
@@ -25,49 +25,49 @@ def home_page(request):
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'red',
-        'Moto':'Peace and Love'
-    },{
+        'Moto': 'Peace and Love'
+    }, {
         'Title': 'Shopin 2',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'green',
-        'Moto':'Love over hate'
-    },{
+        'Moto': 'Love over hate'
+    }, {
         'Title': 'Shopin 3',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'yellow',
-        'Moto':'The Future is yours'
-    },{
+        'Moto': 'The Future is yours'
+    }, {
         'Title': 'Shopin 3',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'yellow',
-        'Moto':'Be The Change'
-    },{
+        'Moto': 'Be The Change'
+    }, {
         'Title': 'Shopin 3',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'yellow',
-        'Moto':'Shop Right'
-    },{
+        'Moto': 'Shop Right'
+    }, {
         'Title': 'Shopin 2',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'green',
-        'Moto':'Shoping Made Easy'
-    },{
+        'Moto': 'Shoping Made Easy'
+    }, {
         'Title': 'Shopin 2',
         'Description': SD_SHOP,
         'Location': 'Tumu',
         'Rating': '5 stars',
         'Color': 'green',
-        'Moto':'Making the sales'
+        'Moto': 'Making the sales'
     }]
     shop = list_of_shops[0]
     shop['Description'] += shop['Title']
@@ -79,12 +79,171 @@ def home_page(request):
             shop['is_top_rated'] = False
         else:
             shop['is_top_rated'] = True
-    
+
     # in other to ensure separation of concerns
     # i will be getting each individual peace of data
     top_rated = [shop for shop in list_of_shops if shop['is_top_rated']][:3]
-    context = {'shop_list':list_of_shops,
-               'top_rated':top_rated,
-               'length':
-               [len(shop['Description']) for shop in list_of_shops]} #context to display to the home page
+    context = {'shop_list': list_of_shops,
+               'top_rated': top_rated,
+               'length':  # context to display to the home page
+               [len(shop['Description']) for shop in list_of_shops]}
     return render(request, 'shopin/index.html', context=context)
+
+# i will need a view to handle specific view of a shop
+
+def view_a_shop(request, shop_id):
+
+    SD_SHOP = 'A very simple shopin template for ' +\
+        'show casing a description to be in the name of laughter'
+    list_of_shops = [{
+        id: 1,
+        'Title': 'Shopin 1',
+        'Description': SD_SHOP + 'This is specific to ',
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'red',
+        'Moto': 'Peace and Love',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 2,
+        'Title': 'Shopin 2',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'green',
+        'Moto': 'Love over hate',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 3,
+        'Title': 'Shopin 3',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'yellow',
+        'Moto': 'The Future is yours',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 4,
+        'Title': 'Shopin 3',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'yellow',
+        'Moto': 'Be The Change',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 5,
+        'Title': 'Shopin 3',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'yellow',
+        'Moto': 'Shop Right',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 6,
+        'Title': 'Shopin 2',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'green',
+        'Moto': 'Shoping Made Easy',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }, {
+        id: 7,
+        'Title': 'Shopin 2',
+        'Description': SD_SHOP,
+        'Location': 'Tumu',
+        'Rating': '5 stars',
+        'Color': 'green',
+        'Moto': 'Making the sales',
+        'Category': {
+            # inside this dictionary are categories
+            # and the items that they can contain
+            'first_cat': {
+                'subsections' : ['first', 'second',
+                                 'third', 'fourth',
+                                 'fifth', 'sixth']
+            },
+            'second_cat': 'second_cat',
+            'third_cat': ['cat1', 'cat2', 'cat3', 'cat4'],
+            'fourth_cat': 'Fourth_cat'
+        }
+    }]
+    """
+        i'm gonna be doing a lot of
+        complex operations
+    """
+    if request.method == 'POST':
+        print('post method accessed')
+    
+    elif request.method == 'GET':
+        print('Get request')
+    
+    return render(request, 'shopin/shop_view.html')
