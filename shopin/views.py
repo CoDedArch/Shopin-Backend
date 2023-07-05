f9171acc7b477db4049caba413a3eee795a0a4e7
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-05 23:33:36 +0000

    Clean Up hard-coded data into model data

--- shopin/views.py
+++ shopin/views.py
@@ -15,8 +15,8 @@ def home_page(request):
         i will not be using an HttpResponse
         but a render
     """
-    SD_SHOP = 'A very simple shopin template for ' +\
-        'show casing a description to be in the name of laughter'
+    SD_SHOP = 'A very simple shopin template for'+\
+        ' show casing a description to be in the name of laughter'
     LD_SHOP = 'A very simple shopin template for '
     'show casing a long description of a shop description'
     list_of_shops = [{
@@ -107,7 +107,7 @@ def view_a_shop(request, shop_id):
             # inside this dictionary are categories
             # and the items that they can contain
             'section1': {
-                'Category': {
+                'Category':[{
                     'first_cat': {
                         'subsections' : [{'name': 'first',
                                         'img': 'image1_url'},
@@ -119,26 +119,91 @@ def view_a_shop(request, shop_id):
                                         'img': 'image4_url'},
                                         {'name': 'fifth',
                                         'img': 'image5_url'}]
-                    },
+                    }},{
                     'second_cat': {
                         'name': 'second_cat',
                         'img': 'second_image_url'
-                    },
+                    }},{
                     'third_cat': {
                         'name': 'third_cat',
                         'img': 'third_img_url'
-                    },
+                    }},{
                     'fouth': {
                         'name': 'fourth_cat',
                         'img': 'fourth_image_url'
-                    },
+                    }},{
                     'fifth': {
                         'name': 'fifth_cat',
                         'img': 'fifth_image_url'  
-                    }
+                    }}
+                ],
+                'color': 'black',
+                'status': {
+                    # meaning that i'll have to get the products of a section.
+                    'contains_only_products': False,
+                    'contains_category': True
                 },
-                'color': 'black'
+                'pagnation_unit': 5
             },
+            'section2': {
+                'Category': [{
+                    'first_cat': {
+                        'name':'',
+                        'img': 'third_urls'
+                     }},
+                    {'second_cat': {
+                        'subsections' : [{'name': 'first',
+                                        'img': 'image1_url'},
+                                        {'name': 'second',
+                                        'img': 'image2_url'},
+                                        {'name': 'third',
+                                        'img': 'image3_url'},
+                                        {'name': 'fourth',
+                                        'img': 'image4_url'},
+                                        {'name': 'fifth',
+                                        'img': 'image5_url'}]
+                    }},
+                    {'third_cat': {
+                        'subsections' : [{'name': 'first',
+                                        'img': 'image1_url'},
+                                        {'name': 'second',
+                                        'img': 'image2_url'},
+                                        {'name': 'third',
+                                        'img': 'image3_url'},
+                                        {'name': 'fourth',
+                                        'img': 'image4_url'},
+                                        {'name': 'fifth',
+                                        'img': 'image5_url'}]
+                    }},
+                    {'fourth_cat': ['cat1', 'cat2', 'cat3', 'cat4']
+                }],
+                'color': 'green',
+                'status': {
+                    # meaning that i'll have to get the products of a section.
+                    'contains_only_products': False,
+                    'contains_category': True
+                },
+                'pagnation_unit': 1
+            },
+            'section3': {
+                'status': {
+                    # meaning that i'll have to get the products of a section.
+                    'contains_only_products': True,
+                    'contains_category': False
+                },
+                'products': [{
+                    'img':'img_url',
+                    'offer': '25% off',
+                    'offer_name': 'Deal',
+                    'name': 'gown',
+                    'price': 'ghs 2000',
+                    'desc':'some long text which will be truncated'
+                }, 'prod2_thumb', 'prod3_thumb',
+                    'prod4_thumb', 'prod5_thumb', 'prod5_thumb'],
+                'section_title':'Under ghc300',
+                'call_to_action':'see more',
+                'pagnation_unit': 2
+            }
         }
     }, {
         'id': 2,
@@ -152,50 +217,45 @@ def view_a_shop(request, shop_id):
             # inside this dictionary are categories
             # and the items that they can contain
             'section1': {
-                'Category': {
-                    'first_cat': 'furst_cat',
-                    'second_cat': {
+                'Category': [{
+                    'first_cat': 'first_cat'},
+                    {'second_cat': {
                         'subsections' : ['first', 'second',
                                         'third', 'fourth',
                                         'fifth', 'sixth']
-                    },
-                    'third_cat': {
+                    }},
+                    {'third_cat': {
                         'subsections' : ['first', 'second',
                                         'third', 'fourth',
                                         'fifth', 'sixth']
-                    },
-                    'fourth_cat': ['cat1', 'cat2', 'cat3', 'cat4']
+                    }},
+                    {'fourth_cat': ['cat1', 'cat2', 'cat3', 'cat4']
+                }],
+                'color': 'red',
+                'status': {
+                    # meaning that i'll have to get the products of a section.
+                    'contains_only_products': False,
+                    'contains_category': True
                 },
-                'color': 'red'
+                'pagnation_unit': 3
             },
-        }
-    }, {
-        'id': 3,
-        'Title': 'Shopin 3',
-        'Description': SD_SHOP,
-        'Location': 'Tarkwa',
-        'Rating': '5 stars',
-        'Color': 'yellow',
-        'Moto': 'The Future is yours',
-        'Sections': {
-            # inside this dictionary are categories
-            # and the items that they can contain
-            'section1': {
-                'Category': {
-                    'first_cat': {
+            'section2': {
+                'Category': [{
+                    'first_cat': 'furst_cat'},
+                    {'second_cat': {
                         'subsections' : ['first', 'second',
                                         'third', 'fourth',
                                         'fifth', 'sixth']
-                    },
-                    'second_cat': 'second_cat',
-                    'third_cat': 'third_cat',
-                    'fourth_cat': {
+                    }},
+                    {'third_cat': {
                         'subsections' : ['first', 'second',
                                         'third', 'fourth',
                                         'fifth', 'sixth']
-                    }   
-                },
-                'color': 'yellow'
+                    }},
+                    {'fourth_cat': ['cat1', 'cat2', 'cat3', 'cat4']
+                }],
+                'color': 'green',
+                'pagnation_unit': 4
             },
         }
     }]
@@ -205,13 +265,16 @@ def view_a_shop(request, shop_id):
     """
     content = None
     if request.method == 'POST':
-        print('post method accessed')
-    
+        print('post method accessed') 
     elif request.method == 'GET':
         for shop in list_of_shops:
             if shop['id'] == int(shop_id):
                 content = shop
-    
+    print(content)
+    for section, section_value in content['Sections'].items():
+        if section_value['status']['contains_category']:
+            for category in section_value['Category']:
+                print(category)
     # i wanna perform some pre logic before 
     return render(request, 'shopin/shop_view.html', content)
 
