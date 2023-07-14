79684a064d95783bc6852a68b89262714f9b74c0
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-14 02:22:39 +0000

    create relationships between models

--- shopin/models.py
+++ shopin/models.py
@@ -15,22 +15,22 @@ class Shop(models.Model):
     """
     PATH_TO_CSV = Path(__file__).resolve().parent.parent
     top_rated = []
-    Title = models.CharField(max_length=100, default='',
+    title = models.CharField(max_length=100, default='',
                              verbose_name='SHP_Name')
-    Description = models.TextField(default='',
+    description = models.TextField(default='',
                                    verbose_name='SHP_Description',
                                    max_length=270)
-    Location = models.CharField(max_length=30, default='',
+    location = models.CharField(max_length=30, default='',
                                 verbose_name='SHP_Location')
-    Rating = models.IntegerField(default=0,
+    rating = models.IntegerField(default=0,
                                  verbose_name='SHP_Rating')
-    Color = models.CharField(max_length=20, default='black')
-    Moto = models.CharField(max_length=100,
+    color = models.CharField(max_length=20, default='black')
+    moto = models.CharField(max_length=100,
                             verbose_name='SHP_Moto')
 
     def __str__(self):
         # return the string representation for each repr
-        return self.Title + '--' + self.Location
+        return self.title + '--' + self.location
     
     __repr__ = __str__
 
@@ -71,6 +71,7 @@ class Shop(models.Model):
            from the all top rated shop
         """
         three_random_top_rated_shop = []
+        # i will have to make sure that this refreshes after a day is over
         for count in range(3):
             random_index = random.randint(0,2)
             three_random_top_rated_shop.append(cls.all_top_rated()[random_index])
@@ -80,32 +81,43 @@ class Shop(models.Model):
 
 
 def upload_image_to(instance, filename):
-    return 'images/%s/%s'%(instance.id, filename)
+    if instance.__class__.__name__ == 'Category':
+        return 'images/category/%s/%s'%(instance.name, filename)
+    elif instance.__class__.__name__ == 'SubCategory':
+        return 'images/subcategory/%s/%s'%(instance.name, filename)
+    
+# have the many relationship
+class Section(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60,
+                            verbose_name='section_name')
+    contains_products_only = models.BooleanField(default=False)
+    contains_category = models.BooleanField()
+    color = models.CharField(max_length=20)
+    pagnation_unit = models.SmallIntegerField()
+    category = models.ManyToManyField('Category', blank=True)
+
+    def save(self, *args, **kwargs):
+        self.contains_products_only = not self.category.exists()
+        self.contains_category = self.category.exists()
+        super(Section, self).save(*args, **kwargs)
+
     
 
-# instantiating
-# Shop.instantiate_SHP_from_csv()
 class Category(models.Model):
     """This repr a category and some functionality that it can contain"""
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     name = models.CharField(max_length=60,
                         verbose_name='Category')
     # read more into working with image field
     image = models.ImageField(upload_to=upload_image_to)
-    has_subsection = models.BooleanField(default=False)
-    section = models.OneToOneField('Section',on_delete=models.CASCADE)
-    # create a custom logic
-    @classmethod
-    def assign_to_nothing(cls):
-        if cls.section.contains_products_only:
-            # retrieve all products from products table
-            # assign to section
-            cls.section['products'] = []
-            
 
-# have the many relationship
-class Section(models.Model):
-    name = models.CharField(max_length=60,
-                            verbose_name='section_name') 
-    contains_products_only = models.BooleanField(default=False)
-    color = models.CharField(max_length=20)
-    pagnation_unit = models.SmallIntegerField()
+    def __str__(self):
+        return f'category-{self.name}'
+    
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to=upload_image_to)
