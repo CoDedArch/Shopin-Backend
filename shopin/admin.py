f77d3061bcfd77286bdbaa59d810d997cfef1fb0
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-14 11:54:48 +0000

    register all models

--- shopin/admin.py
+++ shopin/admin.py
@@ -1,5 +1,8 @@
 """This module helps me register to the admin interface"""
 from django.contrib import admin
-from .models import Shop 
+from . import models as shopin_models 
 # register a model
-admin.site.register(Shop)
+admin.site.register(shopin_models.Shop)
+admin.site.register(shopin_models.Section)
+admin.site.register(shopin_models.Category)
+admin.site.register(shopin_models.SubCategory)
