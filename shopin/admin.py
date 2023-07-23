dbb35fbfbde0c96d134e07851e5c740d650a0292
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-23 14:37:11 +0000

    create an admin to manage each model

--- shopin/admin.py
+++ shopin/admin.py
@@ -1,6 +1,21 @@
 """This module helps me register to the admin interface"""
 from django.contrib import admin
 from . import models as shopin_models 
+
+# we can also have the admin for each model on the site 
+
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+class SectionAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    pass
+
 # register a model
 admin.site.register(shopin_models.Shop)
 admin.site.register(shopin_models.Section)
