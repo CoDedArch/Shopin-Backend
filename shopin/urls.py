d9ccf3e34e89a8ba7ea195f7b99a1b0c4703e201
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-23 13:21:14 +0000

    route to SingleShopView

--- shopin/urls.py
+++ shopin/urls.py
@@ -6,5 +6,5 @@ from django.urls import re_path
 from shopin import views as shopin_views
 urlpatterns = [
     re_path(r'^$', shopin_views.ShopView.as_view(), name='shop'),
-    re_path(r'^(?P<shop_id>\d+)/$', shopin_views.view_a_shop, name='shop')
+    re_path(r'^(?P<shop_id>\d+)/$', shopin_views.SingleShopView.as_view(), name='single_shop')
 ]
