3ca9e3c2e4460835b0bee1735d6358dc9d3b996c
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-23 12:15:51 +0000

    create a route to ShopView

--- shopin/urls.py
+++ shopin/urls.py
@@ -5,6 +5,6 @@
 from django.urls import re_path
 from shopin import views as shopin_views
 urlpatterns = [
-    re_path(r'^$', shopin_views.home_page, name='index'),
+    re_path(r'^$', shopin_views.ShopView.as_view(), name='shop'),
     re_path(r'^(?P<shop_id>\d+)/$', shopin_views.view_a_shop, name='shop')
 ]
