0366468bd648798be6de9a5838200362419800d2
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-27 09:01:33 +0000

    model customer and reviews

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from Products.models import Product
 # Create your models here.
+# customer will have to inherit from user model
+class Customer(User, models.Model):
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+
+class Reviews(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    reviewContent = models.TextField(verbose_name='content for review')
+    rating = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
