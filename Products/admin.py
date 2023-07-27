bc4a23676d6ae5224fa8a40e96858e04ef5aac69
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-07-27 10:25:13 +0000

    register models to admin site

--- Products/admin.py
+++ Products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import (Product, ShoppingCart, Orders)
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    pass
+
+class OrdersAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Orders, OrdersAdmin)
--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Customer, Reviews)
 
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
