dd56c7e1b9f5a370e8e7634ca58bea1bd2414d06
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-08-03 00:17:06 +0000

    initial view for products

--- Products/views.py
+++ Products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Product
 # Create your views here.
+
+class ViewProducts(View):
+    def get(self, request, shop_name):
+        # get all products related to a particular shop
+        all_products = Product.objects.filter(shop = shop_name)
+        print(all_products)
+        return render(request, template_name='products/products.html')
