10aa2056812f41e61d82c6ddb834ac236cd169b7
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-08-03 00:33:03 +0000

    Succesfully fetch the products of a shop

--- Products/views.py
+++ Products/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Product
+from shopin.models import Shop
 # Create your views here.
 
 class ViewProducts(View):
     def get(self, request, shop_name):
         # get all products related to a particular shop
-        all_products = Product.objects.filter(shop = shop_name)
+        try:
+            requested_shop = Shop.objects.get(title=shop_name)
+        except Shop.DoesNotExist:
+            pass
+
+        all_products = Product.objects.filter(shop = requested_shop)
         print(all_products)
         return render(request, template_name='products/products.html')
