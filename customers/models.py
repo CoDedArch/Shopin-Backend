a531a70f9817a99567b2662e6c54eedecb2d7a0d
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-08-05 14:24:08 +0000

    document models and functions

--- customers/models.py
+++ customers/models.py
@@ -4,12 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 # customer will have to inherit from user model
 class Customer(User, models.Model):
+    """Customer inherits from the user model and models a customer object"""
+
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     
 
 
 class Reviews(models.Model):
+    """Reviews, a customer can review a product"""
+    
     reviewContent = models.TextField(verbose_name='content for review')
     rating = models.IntegerField()
     product = models.ForeignKey('Products.Product', on_delete=models.CASCADE, null=True)
