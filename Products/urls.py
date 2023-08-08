77877b0a21d9b4605670f4c3076617c4f274d892
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-08-08 22:19:37 +0000

    url to details of a product

--- Products/urls.py
+++ Products/urls.py
@@ -1,5 +1,6 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from . import views as product_views
 urlpatterns = [
-    re_path(r'^$',view=product_views.ViewProducts.as_view(),name='all_products')
+    re_path(r'^$',view=product_views.ViewCategoryProducts.as_view(),name='all_products'),
+    re_path(r'(?P<product_name>\w+)/', view=product_views.ProductDetailView.as_view(), name='details')    
 ]
