190d5ed0134b56c78819953589c28ca2ff7a8b73
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-08-11 01:37:03 +0000

    register Brand and brand admin

--- Products/admin.py
+++ Products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (Product, ShoppingCart, Order)
+from .models import (Product, ShoppingCart, Order, Brand)
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -11,7 +11,10 @@ class ShoppingCartAdmin(admin.ModelAdmin):
 class OrdersAdmin(admin.ModelAdmin):
     pass
 
+class BrandAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ShoppingCart, ShoppingCartAdmin)
 admin.site.register(Order, OrdersAdmin)
+admin.site.register(Brand, BrandAdmin)
